24414c2b9b538fba9f5000606e50ba7af75e475e
武攀 <wu_pan123@126.com>
2020-12-18 15:23:27 +0800

    日常备份

--- Wp.WpfDemo/Wp.WpfDemo.Model/Enums/Navigation.cs
+++ Wp.WpfDemo/Wp.WpfDemo.Model/Enums/Navigation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace Wp.WpfDemo.Model.Enums
 {
     public enum ENavigationItem
     {
+        [Description("主页面")]
+        Home,
+
+        [Description("联系我们")]
+        ContactUs,
     }
 }
--- Wp.WpfDemo/Wp.WpfDemo.UIL/Views/Pages/HomePage.xaml.cs
+++ Wp.WpfDemo/Wp.WpfDemo.UIL/Views/Pages/HomePage.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Wp.WpfDemo.UIL
+namespace Wp.WpfDemo.UIL.Views.Pages
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// HomePage.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomePage : Page
     {
-        public MainWindow()
+        public HomePage()
         {
             InitializeComponent();
         }

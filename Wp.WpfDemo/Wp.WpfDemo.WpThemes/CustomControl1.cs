a6b1a7637e7a81a40dc9a605f045b132d935e40b
WuPan <wu_pan123@126.com>
2021-04-23 15:40:33 +0800

    日常备份

---
+++ Wp.WpfDemo/Wp.WpfDemo.WpThemes/CustomControl1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wp.WpfDemo.WpThemes
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Wp.WpfDemo.WpThemes"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Wp.WpfDemo.WpThemes;assembly=Wp.WpfDemo.WpThemes"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:CustomControl1/>
+    ///
+    /// </summary>
+    public class CustomControl1 : Control
+    {
+        #region 属性、字段
+
+        public SolidColorBrush TestColor
+        {
+            get => (SolidColorBrush)GetValue(TestColorProperty);
+            set => SetValue(TestColorProperty, value);
+        }
+
+        public static readonly DependencyProperty TestColorProperty
+            = DependencyProperty.Register(nameof(TestColor), typeof(SolidColorBrush), typeof(CustomControl1), new PropertyMetadata(default(SolidColorBrush)));
+
+        #region 命令
+
+        //
+
+        #endregion 命令
+
+        #endregion 属性、字段
+
+        #region 事件
+
+        //
+
+        #endregion 事件
+
+        #region 方法
+
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+
+        #endregion 方法
+    }
+}
